938eb7b2bed545fd60a45391f6b899451c01fa14
mikebenavides29 <135925184+mikebenavides29@users.noreply.github.com>
2025-02-10 10:10:44 -0500

    Update SleepHealth.sql

--- SleepHealth.sql
+++ SleepHealth.sql
@@ -26,12 +26,12 @@ select age, daily_steps, stress_level from sleephealth
 where stress_level >6 and daily_steps >6000
 order by daily_steps desc;
 
---Which occupation as the most normal bmi
+--Which occupation has the most normal bmi
 select occupation, count(bmi_category) from sleephealth
 where bmi_category = 'Normal'
 group by occupation;
 
---Which occupation as the highest quality of sleep
+--Which occupation has the highest quality of sleep
 select occupation, count(quality_of_sleep) from sleephealth
 where quality_of_sleep > 7
 group by occupation;
